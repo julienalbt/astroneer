c31f32b9e7b432ce0634a1b4b3c4c827e0cf669e
julienalbt <alombertjulien@gmail.com>
2019-02-24 00:03:39 +0100

    Ajout machines
    Ajout machines + images objets

--- src/Entity/Machines.php
+++ src/Entity/Machines.php
@@ -5,9 +5,14 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MachinesRepository")
+ * @Vich\Uploadable
  */
 class Machines
 {
@@ -26,7 +31,7 @@ private $name;
 /**
  * @ORM\ManyToMany(targetEntity="App\Entity\Ressources", mappedBy="whatMachines")
  */
-private $ressourecsForMachine;
+private $ressourcesForMachine;
 
 /**
  * @ORM\OneToMany(targetEntity="App\Entity\Vehicles", mappedBy="machineWhoCreateVehicles")
@@ -49,9 +54,38 @@ private $machineWhoCreateMachines;
  */
 private $machinesByMachine;
 
+/**
+ *
+ * @Vich\UploadableField(mapping="machines_images", fileNameProperty="imageName", size="imageSize")
+ *
+ * @var File
+ */
+private $imageFile;
+
+/**
+ * @ORM\Column(type="string", length=100, nullable=true)
+ *
+ * @var string
+ */
+private $imageName;
+
+/**
+ * @ORM\Column(type="integer", nullable=true)
+ *
+ * @var integer
+ */
+private $imageSize;
+
+/**
+ * @ORM\Column(type="datetime", nullable=true)
+ *
+ * @var \DateTime
+ */
+private $updatedAt;
+
 public function __construct()
 {
-$this->ressourecsForMachine = new ArrayCollection();
+$this->ressourcesForMachine = new ArrayCollection();
 $this->vehiclesByMachine = new ArrayCollection();
 $this->objectsByMachine = new ArrayCollection();
 $this->machinesByMachine = new ArrayCollection();
@@ -77,26 +111,26 @@ return $this;
 /**
  * @return Collection|Ressources[]
  */
-public function getRessourecsForMachine(): Collection
+public function getRessourcesForMachine(): Collection
 {
-return $this->ressourecsForMachine;
+return $this->ressourcesForMachine;
 }
 
-public function addRessourecsForMachine(Ressources $ressourecsForMachine): self
+public function addRessourcesForMachine(Ressources $ressourcesForMachine): self
 {
-if (!$this->ressourecsForMachine->contains($ressourecsForMachine)) {
-$this->ressourecsForMachine[] = $ressourecsForMachine;
-$ressourecsForMachine->addWhatMachine($this);
+if (!$this->ressourcesForMachine->contains($ressourcesForMachine)) {
+$this->ressourcesForMachine[] = $ressourcesForMachine;
+$ressourcesForMachine->addWhatMachine($this);
 }
 
 return $this;
 }
 
-public function removeRessourecsForMachine(Ressources $ressourecsForMachine): self
+public function removeRessourcesForMachine(Ressources $ressourcesForMachine): self
 {
-if ($this->ressourecsForMachine->contains($ressourecsForMachine)) {
-$this->ressourecsForMachine->removeElement($ressourecsForMachine);
-$ressourecsForMachine->removeWhatMachine($this);
+if ($this->ressourcesForMachine->contains($ressourcesForMachine)) {
+$this->ressourcesForMachine->removeElement($ressourcesForMachine);
+$ressourcesForMachine->removeWhatMachine($this);
 }
 
 return $this;
@@ -206,4 +240,50 @@ $machinesByMachine->setMachineWhoCreateMachines(null);
 
 return $this;
 }
+
+/**
+ * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+ * of 'UploadedFile' is injected into this setter to trigger the update. If this
+ * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+ * must be able to accept an instance of 'File' as the bundle will inject one here
+ * during Doctrine hydration.
+ *
+ * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $imageFile
+ */
+public function setImageFile(?File $imageFile = null): void
+{
+$this->imageFile = $imageFile;
+
+if (null !== $imageFile) {
+// It is required that at least one field changes if you are using doctrine
+// otherwise the event listeners won't be called and the file is lost
+$this->updatedAt = new \DateTimeImmutable();
+}
+}
+
+public function getImageFile(): ?File
+{
+return $this->imageFile;
+}
+
+public function setImageName(?string $imageName): void
+{
+$this->imageName = $imageName;
+}
+
+public function getImageName(): ?string
+{
+return $this->imageName;
+}
+
+public function setImageSize(?int $imageSize): void
+{
+$this->imageSize = $imageSize;
+}
+
+public function getImageSize(): ?int
+{
+return $this->imageSize;
+}
+
 }
--- src/Form/MachinesType.php
+++ src/Form/MachinesType.php
@@ -3,25 +3,94 @@
 namespace App\Form;
 
 use App\Entity\Machines;
+use App\Entity\Ressources;
+use App\Entity\Objects;
+use App\Entity\Vehicles;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
-class MachinesType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class MachinesType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('name')
-            ->add('ressourecsForMachine')
-            ->add('machineWhoCreateMachines')
+                ->add('name', TextType::class, array(
+                    'label' => 'Nom de la machine',
+                    'attr' => array('placeholder' => 'ex: Imprimante petite'),
+                ))
+                ->add('ressourcesForMachine', EntityType::class, array(
+                    'class' => Ressources::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                                ->orderBy('e.id', 'ASC');
+                    },
+                    'choice_label' => 'name',
+                    'label' => 'Quelles ressources pour cette machine ?',
+                    'multiple' => 'true',
+                    'required' => false,
+                ))
+                ->add('machineWhoCreateMachines', EntityType::class, array(
+                    'class' => Machines::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                                ->orderBy('e.id', 'ASC');
+                    },
+                    'choice_label' => 'name',
+                    'label' => 'Quelle machine produit cette machine ?',
+                    'required' => false,
+                ))
+                ->add('objectsByMachine', EntityType::class, array(
+                    'class' => Objects::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                                ->orderBy('e.id', 'ASC');
+                    },
+                    'choice_label' => 'name',
+                    'label' => 'Quel(s) objet(s) sont construit par cette machine ?',
+                    'multiple' => true,
+                    'required' => false,
+                ))
+                ->add('vehiclesByMachine', EntityType::class, array(
+                    'class' => Vehicles::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                                ->orderBy('e.id', 'ASC');
+                    },
+                    'choice_label' => 'name',
+                    'label' => 'Quel(s) véhicule(s) sont construit par cette machine ?',
+                    'multiple' => true,
+                    'required' => false,
+                ))
+                ->add('machinesByMachine', EntityType::class, array(
+                    'class' => Machines::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                                ->orderBy('e.id', 'ASC');
+                    },
+                    'choice_label' => 'name',
+                    'label' => 'Quelle(s) machine(s) sont construite par cette machine ?',
+                    'multiple' => true,
+                    'required' => false,
+                ))
+                ->add('imageFile', VichImageType::class, [
+                    'label' => 'Image de la machine',
+                    'required' => false,
+                    'allow_delete' => true,
+                    'download_label' => false,
+                    'download_uri' => false,
+                    'image_uri' => false,
+                ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults([
             'data_class' => Machines::class,
         ]);
     }
+
 }
